33c1a2f0fa823d6fac0ab0ac21a687e3d54d546d
gabrielruggie <69177531+gabrielruggie@users.noreply.github.com>
2023-01-21 23:00:06 -0600

    continued developement on astack about apge

--- src/microservices/astack-authentication/ui/main/src/interfaces/About.jsx
+++ src/microservices/astack-authentication/ui/main/src/interfaces/About.jsx
@@ -3,10 +3,41 @@ import { useState, useEffect } from 'react';
 import NavBar from './components/NavBar'
 
 export default function About() {
-    const [cloudClicked, setCloudClicked] = useState(false);
+    const [techClicked, setTechClicked] = useState(false);
+    const [infraClicked, setInfraClicked] = useState(false);
+    const [apiClicked, setApiClicked] = useState(false);
+    const [releaseClicked, setReleaseClicked] = useState(false);
+
 
     const changeCloudState = () => {
-        setCloudClicked(true)
+        setTechClicked(!techClicked);
+        setInfraClicked(false);
+        setApiClicked(false);
+        setReleaseClicked(false);
+    }
+
+
+    const changeInfraClicked = () => {
+        setTechClicked(false);
+        setInfraClicked(!infraClicked);
+        setApiClicked(false);
+        setReleaseClicked(false);
+    }
+
+
+    const changeApiClicked = () => {
+        setTechClicked(false);
+        setInfraClicked(false);
+        setApiClicked(!apiClicked);
+        setReleaseClicked(false);
+    }
+
+
+    const changeReleaseClicked = () => {
+        setTechClicked(false);
+        setInfraClicked(false);
+        setApiClicked(false);
+        setReleaseClicked(!releaseClicked);
     }
 
     const version = '1.0.1'
@@ -33,25 +64,25 @@ export default function About() {
 
             <div className='m-20 flex'>
                 <aside class="w-64" aria-label="Sidebar">
-                    <div class="px-3 py-4 overflow-y-auto rounded-md bg-gray-50 dark:bg-gray-800 text-2xl font-bold">
-                        <ul class="space-y-10">
+                    <div class="px-3 py-4 overflow-y-auto rounded-md bg-gradient-to-b from-fuchsia-600 to-fuchsia-900 text-2xl font-bold">
+                        <ul class="space-y-12">
                             <li className='nav-item' role='presentation'>
-                                <a onClick={changeCloudState} class="nav-link active flex items-center p-3 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                                    <span class="ml-3">Google Cloud</span>
+                                <a onClick={changeCloudState} class="nav-link active flex items-center p-3 rounded-lg text-white hover:bg-gray-900">
+                                    <span class="ml-3">Tech Stack</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="#tabs-profile" class="flex items-center p-3 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                                <a onClick={changeInfraClicked} class="flex items-center p-3 rounded-lg text-white hover:bg-gray-900">
                                     <span class="flex-1 ml-3 whitespace-nowrap">Infrastructure</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="#" class="flex items-center p-3 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                                <a onClick={changeApiClicked} class="flex items-center p-3 rounded-lg text-white hover:bg-gray-900">
                                     <span class="flex-1 ml-3 whitespace-nowrap">Backend API</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="#" class="flex items-center p-3 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                                <a onClick={changeReleaseClicked} class="flex items-center p-3 rounded-lg text-white hover:bg-gray-900">
                                     <span class="flex-1 ml-3 whitespace-nowrap">Latest Release</span>
                                 </a>
                             </li>
@@ -59,16 +90,41 @@ export default function About() {
                     </div>
                 </aside>
                 <div class="ml-10 tab-content text-white" id="tabs-tabContent">
-                    <div class={"" + (cloudClicked ? 'block' : 'hidden')} >
-                        Tab 1 content
+                    <div class={techClicked ? 'p-10 rounded-lg block bg-gradient-to-b from-fuchsia-600 to-fuchsia-900' : 'hidden'} >
+                        <span className='font-bold text-4xl underline'>Core</span>
+                        <div className='grid grid-cols-2 grid-rows-2'>
+                            <div className='m-1 row-span-1 col-span-1 space-y-4'>
+                                <div className='font-bold text-2xl'>Terraform</div>
+                                <div className='font-bold text-md'>Although the infrastructure of ASTACK Beta is fairly basic, we see the value in learning Infrastructure-As-Code to 
+                                    deploy our resources to Google Cloud. As our framework expands, we will begin to reap the benefits of IAC. Currently, 
+                                    ASTACK uses Terraform to deploy a couple test and beta databases to Google Cloud and we hope that support for deploying cloud run 
+                                    containers is imminent
+                                </div>
+                                <ul className='p-2 font-mono bg-black rounded-md'>
+                                    <li>ymblastack-admin1 demo$ terraform init</li>
+                                    <li className='text-white font-bold'>Initializing the backend...</li>
+                                    <li className='font-bold text-lime-500'>Terraform has been successfully initialized!</li>
+                                </ul>
+                            </div>
+                            <div className='m-1 row-span-1 col-span-1'>
+                                <div className='font-bold text-2xl'>React</div>
+                            </div>
+                            <div className='m-1 row-span-1 col-span-1'>
+                                <div className='font-bold text-2xl'>FastAPI</div>
+                            </div>
+                            <div className='m-1 row-span-1 col-span-1'>
+                                <div className='font-bold text-2xl'>PostgresSQL</div>
+                            </div>
+
+                        </div>
                     </div>
-                    <div class="tab-pane fade" id="tabs-profile" role="tabpanel" aria-labelledby="tabs-profile-tab">
+                    <div class={infraClicked ? 'block' : 'hidden'}>
                         Tab 2 content
                     </div>
-                    <div class="tab-pane fade" id="tabs-messages" role="tabpanel" aria-labelledby="tabs-profile-tab">
+                    <div class={apiClicked ? 'block' : 'hidden'}>
                         Tab 3 content
                     </div>
-                    <div class="tab-pane fade" id="tabs-contact" role="tabpanel" aria-labelledby="tabs-contact-tab">
+                    <div class={releaseClicked ? 'block' : 'hidden'}>
                         Tab 4 content
                     </div>
                 </div>
